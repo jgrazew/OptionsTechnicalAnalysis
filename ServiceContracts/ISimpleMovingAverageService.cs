fc710432d414721a9441d5870f9f53367e865058
Joe Grazewski <jgrazew@gmail.com>
2017-12-29 16:06:17 -0500

    Add files via upload

---
+++ ServiceContracts/ISimpleMovingAverageService.cs
@@ -0,0 +1,13 @@
+﻿using OptionsTechnicalAnalysis.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OptionsTechnicalAnalysis.ServiceContracts
+{
+    public interface ISimpleMovingAverageService
+    {
+        Task<SmaTrendData> GetTrendData(string symbol);
+    }
+}
---
+++ ServiceContracts/ITimeSeriesDataService.cs
@@ -0,0 +1,14 @@
+﻿using OptionsTechnicalAnalysis.Models;
+using OptionsTechnicalAnalysis.RepositoryQueryObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OptionsTechnicalAnalysis.ServiceContracts
+{
+    public interface ITimeSeriesDataService
+    {
+        Task<TimeSeriesDataRoot> GetTimeSeriesData(TimeSeriesDataQueryObject stock);
+    }
+}
