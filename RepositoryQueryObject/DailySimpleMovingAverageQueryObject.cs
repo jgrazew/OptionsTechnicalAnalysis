9d708e9996af9c8c9efdd998b8044ec8dddf7717
Joe Grazewski <jgrazew@gmail.com>
2017-12-29 16:21:36 -0500

    Add files via upload

---
+++ RepositoryQueryObject/DailySimpleMovingAverageQueryObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OptionsTechnicalAnalysis.RepositoryQueryObjects
+{
+    public class DailySimpleMovingAverageQueryObject
+    {
+        public string Symbol { get; set; } = "MSFT";
+        public string TimePeriod { get; set; }
+        //close, open, high or low
+        public string SeriesType { get; set; } = "close";
+    }
+}
---
+++ RepositoryQueryObject/TimeSeriesDataQueryObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OptionsTechnicalAnalysis.RepositoryQueryObjects
+{
+    public class TimeSeriesDataQueryObject
+    {
+        public string Symbol { get; set; } = "MSFT";
+        public string OutputSize { get; set; } = "compact";
+    }
+}
