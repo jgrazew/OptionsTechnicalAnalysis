368a5d4003fc944660d624e9258e8ecaf8300929
Joe Grazewski <jgrazew@gmail.com>
2017-12-29 16:03:52 -0500

    Create TimeSeriesDataRepository.cs

---
+++ Repositories/TimeSeriesDataRepository.cs
@@ -0,0 +1,27 @@
+using Newtonsoft.Json;
+using OptionsTechnicalAnalysis.Models;
+using OptionsTechnicalAnalysis.RepositoryContracts;
+using OptionsTechnicalAnalysis.RepositoryQueryObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace OptionsTechnicalAnalysis.Repositories
+{
+    public class TimeSeriesDataRepository : ITimeSeriesDataRepository
+    {
+        public async Task<TimeSeriesDataRoot> Get(TimeSeriesDataQueryObject model)
+        {
+            var client = new HttpClient();
+            //full outPutSize returns up to 20 years of historical data
+            var task = await client.GetAsync($"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={model.Symbol}&outputsize={model.OutputSize}&apikey=CWTF4OA3ZOH6VSB2");
+            var jsonString = task.Content.ReadAsStringAsync().Result; 
+
+            TimeSeriesDataRoot res = JsonConvert.DeserializeObject<TimeSeriesDataRoot>(jsonString);
+
+            return res;
+        }
+    }
+}
