c768552589946482bf30c678cf72df6eef3f6698
Joe Grazewski <jgrazew@gmail.com>
2017-12-29 16:03:12 -0500

    Create DailySimpleMovingAverageRepository.cs

---
+++ Repositories/DailySimpleMovingAverageRepository.cs
@@ -0,0 +1,28 @@
+using Newtonsoft.Json;
+using OptionsTechnicalAnalysis.Models;
+using OptionsTechnicalAnalysis.RepositoryContracts;
+using OptionsTechnicalAnalysis.RepositoryQueryObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace OptionsTechnicalAnalysis.Repositories
+{
+    public class DailySimpleMovingAverageRepository : IDailySimpleMovingAverageRepository
+    {
+        public async Task<SmaRoot> Get(DailySimpleMovingAverageQueryObject model)
+        {
+            var client = new HttpClient();
+            var task = await client.GetAsync($"https://www.alphavantage.co/query?function=SMA&symbol={model.Symbol}&interval=daily&time_period={model.TimePeriod}&series_type={model.SeriesType}&apikey=CWTF4OA3ZOH6VSB2");
+            var jsonString = task.Content.ReadAsStringAsync().Result;
+
+            //dynamic fyn = JsonConvert.DeserializeObject(jsonString);
+
+            SmaRoot res = JsonConvert.DeserializeObject<SmaRoot>(jsonString);
+
+            return res;
+        }
+    }
+}
