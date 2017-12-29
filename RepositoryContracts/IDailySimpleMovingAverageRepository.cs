9562452e57e2cd46478642ec152fef8c7802ec4d
Joe Grazewski <jgrazew@gmail.com>
2017-12-29 16:00:47 -0500

    Add files via upload

---
+++ RepositoryContracts/IDailySimpleMovingAverageRepository.cs
@@ -0,0 +1,14 @@
+﻿using OptionsTechnicalAnalysis.Models;
+using OptionsTechnicalAnalysis.RepositoryQueryObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OptionsTechnicalAnalysis.RepositoryContracts
+{
+    public interface IDailySimpleMovingAverageRepository
+    {
+        Task<SmaRoot> Get(DailySimpleMovingAverageQueryObject model);
+    }
+}
---
+++ RepositoryContracts/ITimeSeriesDataRepository.cs
@@ -0,0 +1,14 @@
+﻿using OptionsTechnicalAnalysis.Models;
+using OptionsTechnicalAnalysis.RepositoryQueryObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OptionsTechnicalAnalysis.RepositoryContracts
+{
+    public interface ITimeSeriesDataRepository
+    {
+        Task<TimeSeriesDataRoot> Get(TimeSeriesDataQueryObject model);
+    }
+}
